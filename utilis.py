46469165ec1072c00b65747530ba9306eadb35e7
Chirag Limbachia <chirag90in@yahoo.co.in>
2021-02-19 10:55:59 -0500

    USP: EDA

---
+++ utilis.py
@@ -0,0 +1,52 @@
+import numpy as np
+import pandas as pd
+
+## Functions to sample bootstrap replicates
+
+def bootstrap_replicate_1d(data,func):
+    """Generate bootstrap replicate of 1D data."""
+    bs_sample = np.random.choice(data,size=len(data))
+    return func(bs_sample)
+
+
+def draw_bs_reps(data, func,size=1):
+    """ Draw bootstrap replicates."""
+
+    # Initialize array of replicates: bs_replicates
+    bs_replicates = np.empty(size)
+
+    # Generate replicates
+    for i in range(size):
+        bs_replicates[i] = bootstrap_replicate_1d(data,func)
+        
+    return bs_replicates
+
+def permutation_sample(data1, data2):
+    """Generate a permutation sample from two data sets."""
+
+    # Concatenate the data sets: data
+    data = np.concatenate((data1,data2))
+
+    # Permute the concatenated array: permuted_data
+    permuted_data = np.random.permutation(data)
+
+    # Split the permuted array into two: perm_sample_1, perm_sample_2
+    perm_sample_1 = permuted_data[:len(data1)]
+    perm_sample_2 = permuted_data[len(data1):]
+
+    return perm_sample_1, perm_sample_2
+
+def draw_perm_reps(data_1, data_2, func, size=1):
+    """Generate multiple permutation replicates."""
+
+    # Initialize array of replicates: perm_replicates
+    perm_replicates = np.empty(size)
+
+    for i in range(size):
+        # Generate permutation sample
+        perm_sample_1, perm_sample_2 = permutation_sample(data_1,data_2)
+
+        # Compute the test statistic
+        perm_replicates[i] = func(perm_sample_1,perm_sample_2)
+
+    return perm_replicates
